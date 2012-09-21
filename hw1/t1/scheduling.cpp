12c5c862497983ae280599d91399c5dc44a00530
Nick Adams <nwadams@nickadams.ca>
2012-09-20 22:33:56 -0700

    add scheduling class

---
+++ hw1/t1/scheduling.cpp
@@ -0,0 +1,40 @@
+#include "task.h"
+#include "scheduling.h"
+#include <list>
+
+using namespace std;
+
+Scheduling::Scheduling(list<Task> tasks)
+{
+	taskSet = tasks;
+}
+
+void Scheduling::sortRM()
+{
+	
+}
+
+void Scheduling::sortSJF()
+{
+	
+}
+
+void Scheduling::sortMUF()
+{
+	
+}
+
+void Scheduling::LLBoundTest()
+{
+
+}
+
+void Scheduling::hyperbolicBoundTest()
+{
+
+}
+
+void Scheduling::WCRTTest()
+{
+
+}
---
+++ hw1/t1/scheduling.h
@@ -0,0 +1,26 @@
+#ifndef SCHEDULING_H
+#define SCHEDULING_H
+
+#include "task.h"
+#include <list>
+
+using namespace std;
+
+class Scheduling {
+
+	public:
+	list<Task> taskSet;
+	
+	Scheduling(list<Task> tasks);
+	
+	void sortRM();
+	void sortSJF();
+	void sortMUF();
+	
+	void LLBoundTest();
+	void hyperbolicBoundTest();
+	void WCRTTest();
+	
+};
+
+#endif
